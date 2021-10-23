1a02b04da9b5eb31c03572fef1b24eb742844ab4
yigittin <i.yigittin@hotmail.com>
2021-10-24 00:22:42 +0300

    Task on Home Page & Bottom Sheet Buttons

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -5,10 +5,22 @@ import 'package:reminder_app/models/task.dart';
 class TaskController extends GetxController {
   @override
   void onReady() {
+    getTasks();
     super.onReady();
   }
 
+  var taskList = <Task>[].obs;
+
   Future<int> addTask({Task? task}) async {
     return await DBHelper.insert(task);
+  }
+
+  void getTasks() async {
+    List<Map<String, dynamic>> tasks = await DBHelper.query();
+    taskList.assignAll(tasks.map((data) => new Task.fromJson(data)).toList());
+  }
+
+  void delete(Task task) {
+    DBHelper.delete(task);
   }
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,12 +1,16 @@
 import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:reminder_app/controllers/task_controller.dart';
+import 'package:reminder_app/models/task.dart';
 import 'package:reminder_app/services/notification_services.dart';
 import 'package:reminder_app/services/theme_services.dart';
 import 'package:reminder_app/ui/theme.dart';
 import 'package:reminder_app/ui/widgets/button.dart';
+import 'package:reminder_app/ui/widgets/task_tile.dart';
 
 import 'add_task_bar.dart';
 
@@ -19,6 +23,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   DateTime _selectedDate = DateTime.now();
+  final _taskController = Get.put(TaskController());
   @override
   void initState() {
     super.initState();
@@ -38,7 +43,126 @@ class _HomePageState extends State<HomePage> {
             children: [
               _addTaskBar(),
               _addDateBar(),
+              SizedBox(
+                height: 10,
+              ),
+              _showTasks(),
             ],
+          ),
+        ));
+  }
+
+  _showTasks() {
+    return Expanded(child: Obx(() {
+      return ListView.builder(
+          itemCount: _taskController.taskList.length,
+          itemBuilder: (_, index) {
+            return AnimationConfiguration.staggeredList(
+                position: index,
+                child: SlideAnimation(
+                  child: FadeInAnimation(
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            _showBottomSheet(
+                                context, _taskController.taskList[index]);
+                          },
+                          child: TaskTile(_taskController.taskList[index]),
+                        )
+                      ],
+                    ),
+                  ),
+                ));
+          });
+    }));
+  }
+
+  _showBottomSheet(BuildContext context, Task task) {
+    Get.bottomSheet(Container(
+      padding: const EdgeInsets.only(top: 4),
+      height: task.isCompleted == 1
+          ? MediaQuery.of(context).size.height * 0.24
+          : MediaQuery.of(context).size.height * 0.32,
+      color: Get.isDarkMode ? darkGreyClr : Colors.white,
+      child: Column(
+        children: [
+          Container(
+            height: 6,
+            width: 120,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Get.isDarkMode ? Colors.grey[600] : Colors.grey[300]),
+          ),
+          Spacer(),
+          task.isCompleted == 1
+              ? Container()
+              : _bottomSheetButton(
+                  label: "Task Completed",
+                  onTap: () {
+                    Get.back();
+                  },
+                  clr: primaryClr,
+                  context: context),
+          SizedBox(
+            height: 2,
+          ),
+          _bottomSheetButton(
+              label: "Delete Task",
+              onTap: () {
+                Get.back();
+              },
+              clr: Colors.redAccent,
+              context: context),
+          SizedBox(
+            height: 20,
+          ),
+          _bottomSheetButton(
+              label: "Close",
+              onTap: () {
+                Get.back();
+              },
+              clr: Colors.white,
+              isClose: true,
+              context: context),
+          SizedBox(
+            height: 10,
+          )
+        ],
+      ),
+    ));
+  }
+
+  _bottomSheetButton(
+      {required String label,
+      required Function() onTap,
+      required Color clr,
+      bool isClose = false,
+      required BuildContext context}) {
+    return GestureDetector(
+        onTap: onTap,
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 4),
+          height: 55,
+          width: MediaQuery.of(context).size.width * 0.9,
+          decoration: BoxDecoration(
+            border: Border.all(
+                width: 2,
+                color: isClose == true
+                    ? Get.isDarkMode
+                        ? Colors.grey[600]!
+                        : Colors.grey[300]!
+                    : clr),
+            borderRadius: BorderRadius.circular(20),
+            color: isClose == true ? Colors.white : clr,
+          ),
+          child: Center(
+            child: Text(
+              label,
+              style: isClose
+                  ? titleStyle
+                  : titleStyle.copyWith(color: Colors.white),
+            ),
           ),
         ));
   }
@@ -93,7 +217,12 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
-          MyButton(label: "+ Add Task", onTap: () => Get.to(AddTaskPage())),
+          MyButton(
+              label: "+ Add Task",
+              onTap: () async {
+                await Get.to(() => AddTaskPage());
+                _taskController.getTasks();
+              })
         ],
       ),
     );
